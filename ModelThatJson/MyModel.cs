c1de78c075d49e0a63c6ba24e9cc4e188988f129
Kirstin Megga Ramos <kirstinmegga.ramos@ph.softvision.global>
2022-06-09 22:17:45 +0800

    initial commit

---
+++ ModelThatJson/MyModel.cs
@@ -0,0 +1,44 @@
+﻿
+namespace ModelThatJson
+{
+    public class MyModel
+    {
+        public int cid { get; set; }
+        public string entityName { get; set; }
+        public _accounts accounts { get; set; }
+    }
+
+    public class _accounts
+    {
+        public worldInvestor usa { get; set; }
+        public worldInvestor slovakia { get; set; }
+        public worldInvestor ecuador { get; set; }
+    }
+
+    public class worldInvestor
+    {
+        public _EntityStockShares entityStockShares { get; set; }
+    }
+
+    public class _EntityStockShares
+    {
+        public string label { get; set; }
+        public string desc { get; set; }
+        public _units units { get; set; }
+    }
+
+    public class _units
+    {
+        public _trades[] trades { get; set; }
+    }
+
+    public class _trades
+    {
+        public int acct { get; set; }
+        public DateTime dateOfTrade { get; set; }
+        public string ticker { get; set; }
+        public string tranType { get; set; }
+        public int qty { get; set; }
+        public decimal costPerShare { get; set; }
+    }
+}
---
+++ ModelThatJson/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ModelThatJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"C:\Users\kirstinmegga.ramos\source\repos\ModelThatJson\ModelThatJson\sample.json";
+
+            string jsonText = File.ReadAllText(fileName);
+
+            var data = Newtonsoft.Json.JsonConvert.DeserializeObject<MyModel>(jsonText);
+        }
+    }
+}
